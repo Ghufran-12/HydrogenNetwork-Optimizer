31dedc143134db8ce05a576109b5ddb8100b432b
Ghufran <swtcandy785@gmail.com>
2026-05-04 22:53:53 +0300

    fix cors for deployed frontend

--- src/backend/main.py
+++ src/backend/main.py
@@ -33,7 +33,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000",
+    "http://localhost:3001",
+    "https://hydrogennetwork-optimizer-dou9.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
